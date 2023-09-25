f4a92de811fcade80e47a570cafa1637d3501997
Etxebe <mikelgartzia21@gmail.com>
2023-09-25 08:48:31 +0200

    [NEW] añadidos ficheros ingredient, ingredeints y effects.mjs

---
+++ ingredients.mjs
@@ -0,0 +1,25 @@
+import Ingredient from "./ingredient.mjs";
+
+
+
+export default class Ingredients
+{
+    constructor(ingredients)
+    {
+        this.ingredients = ingredients;
+    }
+
+    static load(data) {
+        return new Ingredients(data.map(Ingredient.from));
+    }
+
+    find(name) {
+        const ingredient = this.ingredients.find(element => element.hasName(name));
+        if(ingredient === undefined)
+        throw new Error(`Unknow ingredient ${name}`);
+
+        return ingredient;
+    }
+
+}
+
