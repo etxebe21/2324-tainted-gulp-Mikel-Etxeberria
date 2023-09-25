d7fafef3f9e638443aeacfa1367f222cc477e363
Etxebe <mikelgartzia21@gmail.com>
2023-09-25 09:25:40 +0200

    [NEW]creada funcioń getBags y showINgredients para ver los ingredientes de una bolsa

--- index.mjs
+++ index.mjs
@@ -1,17 +1,44 @@
-import { getData } from "./service.mjs";
-
+import { getData, getBags } from "./service.mjs";
+import Ingredients from "./ingredients.mjs";
+import Cauldron from "./cauldron.mjs";
 
 const execute = async () => {
 
     try
     {
-        const responseData = await getData();
+        const ingredientsData = await getData();
         
-        const data = responseData["ingredients"];
+        const data = ingredientsData["ingredients"];
 
-        const ingredients = Ingredients.load(data);
+        const bagData = await getBags();
+
+        const dataBag = bagData["players"];
+
+        const ingredients = dataBag.pouch_red;
+
+        showIngredients(dataBag);
       
-       
+        const cauldron = new Cauldron(ingredients);
+
+        // const potion1 = cauldron.createPotion("Bear Claws", "Bee");
+        
+
+        // const potion2 = cauldron.createPotion("Chicken's Egg", "Chaurus Eggs");
+      
+        // const potion3 = cauldron.createPotion("Chaurus Eggs", "Bleeding Crown");
+      
+
+        // const potion4 = cauldron.createPotion("Nightshade", "Ectoplasm");
+        
+
+        // const potions = [];
+
+        // potions.push(potion1);
+        // potions.push(potion2);
+        // potions.push(potion3);
+        // potions.push(potion4);
+
+        // console.log(potions);
 
     }
     catch(error)
@@ -22,3 +49,14 @@ const execute = async () => {
 }
 
 execute();
+
+const showIngredients = (dataBag) => {
+
+    dataBag.forEach(element => {
+        const effects = element.pouch_red;
+        console.log(` Bolsa: ${effects}`);
+        
+    });
+
+}
+
